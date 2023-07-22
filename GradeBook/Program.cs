8a7ab87c3cf17c9249ce4700efb7e42d2711ba4a
Łukasz Ryczko <lukasz.ryczko@fingoweb.com>
2023-07-22 22:59:05 +0200

    initial commit

---
+++ GradeBook/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using GradeBook.UserInterfaces;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start");
+            StartingUserInterface.CommandLoop();
+            
+            Console.WriteLine("Stop!");
+            Console.Read();
+        }
+    }
+}
